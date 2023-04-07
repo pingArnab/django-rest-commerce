1ada77b12f17ec928f2e0cfdd29d2a254fcaee77
arnab <ping.arnab@gmail.com>
2023-04-08 00:55:33 +0530

    redirecting Admin Logout to common logout

--- DRC/urls.py
+++ DRC/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import RedirectView
 from django.views.static import serve
 
 
 urlpatterns = [
+    path('admin/logout/', RedirectView.as_view(url='/logout/', permanent=False), name='admin_logout'),
     path('admin/', admin.site.urls, name='ADMIN'),
     path('api/v1/', include('DRC.api_urls')),
     path('', include('HOME.urls')),
